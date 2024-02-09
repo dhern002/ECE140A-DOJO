2b026138241795e62ce60157fd2f5c068e64cf67
Dominic Orlando <dorlando@ucsd.edu>
2024-02-08 21:10:59 -0800

    Finished clock addition demo

--- static/js/domdemo.js
+++ static/js/domdemo.js
@@ -1,3 +1,63 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    console.log(event);
+    let update_interval = setInterval(update_time, 1000);
+    let clock_holder = document.querySelector('#clock_holder');
+    let num_clocks = 0;
+    let indices = [];
+    document.querySelector('#add').addEventListener("submit", (event) => {
+       event.preventDefault();
+       let input = event.target.querySelector(`input[type='text']`).value;
+       add_clock(input);
+       event.target.reset();
+    });
+
+    document.querySelector('#remove').addEventListener('submit', (event) => {
+       event.preventDefault();
+       let input = parseInt(event.target.querySelector(`input[type='number']`).value, 10);
+       if(input >= 0 && input < indices.length) {
+           remove_clock(input);
+       } else {
+           alert('Invalid clock!');
+       }
+       event.target.reset();
+    });
+
+    function add_clock(timezone) {
+        let new_clock = document.querySelector('template').content.cloneNode(true);
+        num_clocks++;
+        new_clock.querySelector('strong').innerText = timezone;
+        new_clock.querySelector('strong').setAttribute('data-offset', Math.floor(Math.random() * 20) - 10);
+        new_clock.querySelector('article').id = `clock${num_clocks}`;
+        indices.push(num_clocks);
+        let date = new Date;
+        let time = new_clock.querySelector('time');
+        new_clock.querySelector('time').innerText = (date.toLocaleTimeString());
+        clock_holder.appendChild(new_clock);
+    };
+
+    function remove_clock(index) {
+        let clock_number = indices[index];
+        document.querySelector('#clock_holder').removeChild(document.querySelector(`#clock${clock_number}`));
+        indices.splice(index, 1);
+        num_clocks--;
+    };
+
+    function update_time() {
+        let clocks = document.querySelectorAll('.clock');
+        clocks.forEach((x) => {
+            let timezone_offset = x.querySelector('strong').getAttribute('data-offset');
+            let date = new Date();
+            x.querySelector('time').innerText = getTime(timezone_offset);
+        });
+    }
+
+    function getTime(timezone) {
+        let options = {
+                timeZone: timezone,
+                hour: 'numeric',
+                minute: 'numeric',
+                second: 'numeric',
+            },
+            formatter = new Intl.DateTimeFormat([], options);
+        return formatter.format(new Date());
+    }
 });
